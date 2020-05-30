b876e595ec71900d3353f4fa8adcb4678f841982
Anirudh Kaushik <anirkkau@usc.edu>
2020-05-29 18:31:53 -0700

    added conditional rendering of timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import QuestionComponent from './components/QuestionComponent';
 import { Button } from 'antd';
 import DynamicFieldSet from './components/GoalsInputComponent';
+import PomoTimer from './components/PomoTimer';
 
 function App() {
+	const [ timerOn, setTimerOn ] = useState(false);
+	function toggleTimerOn() {
+		setTimerOn(!timerOn);
+	}
 	return (
 		<div className="App-container">
 			<div className="App-banner">Deepflow</div>
 			<div className="App">
 				<QuestionComponent questionString="Is your workspace clear of all distractions?" />
 				<QuestionComponent questionString="What is your goal?" />
-				<DynamicFieldSet />
 				<QuestionComponent questionString="How long is your session?" />
 
-				<Button type="primary">Start Session</Button>
+				<Button type="primary" onClick={() => toggleTimerOn()}>
+					Start Session
+				</Button>
+				<br />
+				{timerOn ? <div /> : <PomoTimer />}
 			</div>
 		</div>
 	);
---
+++ src/components/PomoTimer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PomoTimer.css';
+
+function PomoTimer(props: any) {
+	return (
+		<div>
+			<div className="circleBase">
+				<div id="timer-text">Timer</div>
+			</div>
+		</div>
+	);
+}
+
+export default PomoTimer;
