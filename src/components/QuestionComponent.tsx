d8abd6533a075af768568f6fa6391d0e889c0aa0
Anirudh Kaushik <anirkkau@usc.edu>
2020-06-02 19:02:37 -0700

    fixed q coloring

--- src/components/QuestionComponent.tsx
+++ src/components/QuestionComponent.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './QuestionComponent.css';
+import 'antd/dist/antd.css';
+import { Form, Input, Button, Radio, Select, Cascader, DatePicker, InputNumber, TreeSelect, Switch } from 'antd';
 
 function QuestionComponent(props: any) {
+	const [ borderColor, setBorderColor ] = useState('grey');
+	const [ clicked, setClicked ] = useState(false);
+
+	function toggleBorderColor() {
+		clicked ? setBorderColor('grey') : setBorderColor('green');
+		setClicked(!clicked);
+	}
+	function toggleMouseBorderColor(action: string) {
+		if (action == 'enter' && !clicked) {
+			setBorderColor('yellow');
+		}
+		if (action == 'exit' && !clicked) {
+			setBorderColor('grey');
+		}
+	}
 	return (
 		<div>
-			<div className="Question-box">{props.questionString}</div>
+			<div
+				className="Question-box"
+				style={{ borderColor: borderColor }}
+				onClick={() => toggleBorderColor()}
+				onMouseEnter={() => toggleMouseBorderColor('enter')}
+				onMouseLeave={() => toggleMouseBorderColor('exit')}
+			>
+				<span>{props.questionString}</span>
+			</div>
 			<br />
 		</div>
 	);
